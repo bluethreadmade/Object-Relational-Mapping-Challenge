c222fd8bbd27a0881d23788d749332fb0bdcd0cb
Megan McNeill <mcneim2@gmail.com>
2024-07-10 16:35:57 -0400

    added got all products route

--- models/index.js
+++ models/index.js
@@ -13,14 +13,16 @@ Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    unique: false
   }
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    unique: false
   }
 })
 
