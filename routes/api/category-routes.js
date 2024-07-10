5646f277abbfbcd7ae10abc735afbb517fb2da4b
Megan McNeill <mcneim2@gmail.com>
2024-07-10 15:24:08 -0400

    created get all and get one by id routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,40 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try{
-    const categoryData = Category.findAll({include: Product});
+    const categoryData = await Category.findAll({include: Product});
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const oneCategory = await Category.findByPk(req.params.id, {include: Product});
+  
+
+  if (!oneCategory) {
+    res.status(404).json({ message: "No category found with this id"});
+    return;
+  }
+
+  res.status(200).json(oneCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = Category.create(req.body);
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
